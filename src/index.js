990fec37ae40e68dd2365769b2fea251d0279f7d
Josep M Sobrepere <josepot@gmail.com>
2019-03-13 16:23:48 +0100

    Separate Client and Server Providers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import Provider from './Provider'
+import Provider, { ServerProvider } from './Provider'
 import connect from './connect'
 import useRedux from './useRedux'
 import useReduxActions from './useReduxActions'
 import useReduxState from './useReduxState'
 
-export { Provider, connect, useRedux, useReduxActions, useReduxState }
+export {
+  Provider,
+  ServerProvider,
+  connect,
+  useRedux,
+  useReduxActions,
+  useReduxState
+}
